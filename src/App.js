4e1625816c7ef98e4fa5e457712f6598d6b86d56
Thiago <thiago.ctecnico@gmail.com>
2023-10-25 06:38:51 -0300

    start commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWord';
 
 function App() {
+
+
+  const name = 'Thiago'
+  const newName = name.toUpperCase()
+  function sum(a,b){
+    return a + b
+  }
+  const url = "https://img.freepik.com/vetores-gratis/fundo-de-luxo-de-ouro_52683-43998.jpg?t=st=1693584048~exp=1693584648~hmac=e77cb60c186397f1da9878c9f16f4db876f52c2f9ad3c1216c07712153873327"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React</h1>
+      <p>Hello, {newName}</p>
+      <p>soma: {sum(1,3)}</p>
+      <img src={url} alt="imagem"/>
+      <HelloWorld />
     </div>
   );
 }
---
+++ src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase(){
+    return(
+        <div>
+            <p>Componente com frase</p>
+        </div>
+    )
+}
+
+export default Frase;
